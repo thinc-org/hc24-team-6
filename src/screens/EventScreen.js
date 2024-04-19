bc493d118fdf68bfa92bccf99803fd7579b71ed4
MoosaTae <sadhis.tae@gmail.com>
2024-04-20 01:09:22 +0700

    Add event screen layout and assets

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, SafeAreaView } from "react-native";
+import React from "react";
 
 export default function EventScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>EventScreen</Text>
+  return (
+    <SafeAreaView className=" w-screen h-screen bg-background">
+      <View className="flex-1 items-center space-y-[5%] w-full h-full px-[2%]">
+        <View className="w-full aspect-square">
+          <Image
+            className="w-full h-full rounded-3xl"
+            resizeMethod="resize"
+            source={require("../assets/event.jpeg")}
+          />
         </View>
-      );
-    }
+        <View className="bg-white p-3 w-full rounded-xl shadow-md flex flex-row">
+          <View className="flex flex-col flex-wrap mt-4">
+            <Text className="font-semibold text-[16px]">
+              งานกินเลี้ยงจิบน้ำชา
+            </Text>
+            <View className="w-full mt-auto flex flex-col">
+
+              <Text className="text-slate-600 text-[10px]">
+                17 May 2023
+              </Text>
+            </View>
+          </View>
+          <View className="h-20 w-20 ml-auto">
+            <Image
+              className="w-full h-full rounded-xl"
+              source={require("../assets/event.jpeg")}
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./App.js", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#F1F1F1",
+        "gradient-start": "#FD297B",
+        "gradient-middle": "#FF5864",
+        "gradient-end": "#FF655B",
+      },
+    },
   },
   plugins: [],
-}
+};

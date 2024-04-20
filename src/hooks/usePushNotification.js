e84aea788a32c8741cd1b12a5a9abc2b0aa5d4dd
Pongsaky <pongsakon.kaew@gmail.com>
2024-04-20 22:39:01 +0700

    SchedulePushNotification Adding

--- src/hooks/usePushNotification.js
+++ src/hooks/usePushNotification.js
@@ -1,10 +1,18 @@
 import { useState, useEffect, useRef } from "react";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
+
 import Constants from "expo-constants";
+
 import { Platform } from "react-native";
 
-export const usePushNotification = () => {
+// export interface PushNotificationState {
+//     expoPushToken?: Notifications.ExpoPushToken;
+//     notification?: Notifications.Notification;
+// }
+
+export function usePushNotifications() {
+    
     Notifications.setNotificationHandler({
         handleNotification: async () => ({
             shouldPlaySound: false,
@@ -20,57 +28,61 @@ export const usePushNotification = () => {
     const notificationListener = useRef();
     const responseListener = useRef();
 
-    async function registerForPushNotificationAsync() {
+    async function registerForPushNotificationsAsync() {
         let token;
-
         if (Device.isDevice) {
-            const { status: existingStatus } = await Notifications.getPermissionsAsync();
-
+            const { status: existingStatus } =
+                await Notifications.getPermissionsAsync();
             let finalStatus = existingStatus;
 
             if (existingStatus !== "granted") {
                 const { status } = await Notifications.requestPermissionsAsync();
                 finalStatus = status;
             }
-
             if (finalStatus !== "granted") {
-                alert("Failed to get push token");
+                alert("Failed to get push token for push notification");
+                return;
             }
 
             token = await Notifications.getExpoPushTokenAsync({
                 projectId: Constants.expoConfig?.extra?.eas.projectId,
             });
-
-            if (Platform.OS === "android") {
-                Notifications.setNotificationChannelAsync("default", {
-                    name: "default",
-                    importance: Notifications.AndroidImportance.MAX,
-                    vibrationPattern: [0, 250, 250, 250],
-                    lightColor: "#FF231F7C",
-                });
-            }
-
-            return token;
         } else {
-            console.log("ERROR: Please use a physical device");
+            alert("Must be using a physical device for Push notifications");
         }
+
+        if (Platform.OS === "android") {
+            Notifications.setNotificationChannelAsync("default", {
+                name: "default",
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: "#FF231F7C",
+            });
+        }
+
+        return token;
     }
 
     useEffect(() => {
-        registerForPushNotificationAsync().then((token) => {
+        registerForPushNotificationsAsync().then((token) => {
             setExpoPushToken(token);
         });
 
-        notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
-            setNotification(notification);
-        });
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener((notification) => {
+                setNotification(notification);
+            });
 
-        responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
-            console.log(response);
-        });
+        responseListener.current =
+            Notifications.addNotificationResponseReceivedListener((response) => {
+                console.log(response);
+            });
 
         return () => {
-            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(
+                notificationListener.current
+            );
+
             Notifications.removeNotificationSubscription(responseListener.current);
         };
     }, []);

e81e8aec98f68167a6a813ccbbd0a0cd8e92dcc7
Pongsaky <pongsakon.kaew@gmail.com>
2024-04-20 14:01:32 +0700

    Revert "Add test Click notification"
    This reverts commit e1057e0666a88ed3c622f134089a443e6b90d923.

--- App.js
+++ App.js
@@ -1,46 +1,7 @@
-import { SafeAreaView } from 'react-native';
 import AppNavigation from './src/navigation';
-import notifee from "@notifee/react-native";
-
 export default function App() {
-
-  // define our handler function:
-  async function onDisplayNotification() {
-
-    // Request permissions (required for IOS)
-    await notifee.requestPermission();
-
-    // Create a channel (required for Andriod)
-    const channelId = await notifee.createChannel({
-      id: "default",
-      name: "Default Channel",
-    });
-
-    // Display a notification
-    await notifee.displayNotification({
-      title: "Notification Title",
-      body: "Main body content of the notification",
-      android: {
-        channelId,
-        // pressActuin is needed if you want the notification to open the app when pressed
-        pressAction: {
-          id: "default"
-        },
-      },
-    });
-
-  }
-
-
   return (
-    // <AppNavigation />
-    <SafeAreaView>
-      {/*Create a button, that when clicked, should display a notification to the user*/}
-      <Button
-        title="Display Notification"
-        onPress={() => onDisplayNotification()}
-      />
-    </SafeAreaView>
+    <AppNavigation />
   );
 }
 
